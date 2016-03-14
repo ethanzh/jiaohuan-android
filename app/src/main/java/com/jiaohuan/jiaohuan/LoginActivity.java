23da2580b38ec33fdfca8e7c835098e114950d92
Ethan Houston <ethan.houston@gmail.com>
2016-03-14 22:26:34 +0800

    Kept working on translucency

--- app/src/main/java/com/jiaohuan/jiaohuan/LoginActivity.java
+++ app/src/main/java/com/jiaohuan/jiaohuan/LoginActivity.java
@@ -6,6 +6,8 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -73,11 +75,14 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private View mLoginFormView;
     private TextView mCreateAcccount;
     private TextView mNeedHelp;
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //mButton.getBackground().setColorFilter(Color.parseColor("#000000"), PorterDuff.Mode.MULTIPLY);
 
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
