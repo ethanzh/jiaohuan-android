5992a0f24b0574080816ef21c881df4e65634e0e
Ethan Houston <ethan.houston@gmail.com>
2016-03-24 21:06:28 +0800

    Started on my_cards

--- app/src/main/java/com/jiaohuan/jiaohuan/MyCards.java
+++ app/src/main/java/com/jiaohuan/jiaohuan/MyCards.java
@@ -7,11 +7,29 @@ import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MyCards extends android.support.v4.app.Fragment {
-    @Nullable
+
+    private ListView mListView;
+    private Button mButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.my_cards, container, false);
+        //return inflater.inflate(R.layout.my_cards, container, false);
+
+        View view = inflater.inflate(R.layout.my_cards, container, false);
+
+        mListView = (ListView) view.findViewById( R.id.list_view );
+
+        //ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, R.layout.my_cards, );
+
+
+        return view;
     }
 }
