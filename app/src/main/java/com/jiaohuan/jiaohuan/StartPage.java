b65170380e0610409c31e792affd74881cece132
Ethan Houston <ethan.houston@gmail.com>
2016-04-30 21:06:19 +0800

    Somewhat fixed contact permissions

--- app/src/main/java/com/jiaohuan/jiaohuan/StartPage.java
+++ app/src/main/java/com/jiaohuan/jiaohuan/StartPage.java
@@ -1,24 +1,25 @@
 package com.jiaohuan.jiaohuan;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
-
+import android.widget.Toast;
 
 public class StartPage extends Activity {
 
     LinearLayout mLinearLayout;
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,22 +28,17 @@ public class StartPage extends Activity {
 
         mLinearLayout = (LinearLayout) findViewById(R.id.start_layout);
 
-        //mLinearLayout.setBackgroundResource(R.drawable.blurred_shanghai_startpage);
-        mLinearLayout.setBackgroundColor(Color.parseColor("#0e0019"));
-
         Drawable background = getDrawable(StartPage.this, R.drawable.hk);
 
         mLinearLayout.setBackground(background);
-
 
         // Start sign in process
         Button signin = (Button) findViewById(R.id.signin);
         signin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                checkContactPermissions(0);
 
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivityForResult(intent, 1);
             }
         });
 
@@ -51,9 +47,8 @@ public class StartPage extends Activity {
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                checkContactPermissions(1);
 
-                Intent intent = new Intent(getApplicationContext(), CreateAccountCard.class);
-                startActivityForResult(intent, 2);
             }
         });
     }
@@ -65,5 +60,59 @@ public class StartPage extends Activity {
             return context.getResources().getDrawable(id);
         }
     }
+    private void checkContactPermissions(int requestCode) {
+        int hasWriteContactsPermission = 0;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            hasWriteContactsPermission = checkSelfPermission(Manifest.permission.WRITE_CONTACTS);
+        }
+        if (hasWriteContactsPermission != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[] {Manifest.permission.WRITE_CONTACTS},
+                        REQUEST_CODE_ASK_PERMISSIONS);
+            }
+            return;
+        }
 
+        if(requestCode == 0){
+            login();
+        } else if(requestCode == 1){
+            signup();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_PERMISSIONS:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission Granted
+                    if(requestCode == 0){
+                        Log.wtf("ACCEPTED", "HELLO");
+                        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                        startActivityForResult(intent, requestCode);
+                    }else if(requestCode == 1){
+                        Intent intent = new Intent(getApplicationContext(), CreateAccountCard.class);
+                        startActivityForResult(intent, requestCode);
+                    }
+
+                } else {
+                    // Permission Denied
+                    Toast.makeText(StartPage.this, "WRITE_CONTACTS Denied", Toast.LENGTH_SHORT)
+                            .show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    public void login(){
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivityForResult(intent, 0);
+    }
+
+    public void signup(){
+        Intent intent = new Intent(getApplicationContext(), CreateAccountCard.class);
+        startActivityForResult(intent, 1);
+    }
 }
