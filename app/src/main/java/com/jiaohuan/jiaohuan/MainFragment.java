a33bc245724062a3efdf32b7ca18216fe9bf7b5e
Ethan Houston <ethan.houston@gmail.com>
2016-08-05 22:28:13 +0800

    friendship

--- app/src/main/java/com/jiaohuan/jiaohuan/MainFragment.java
+++ app/src/main/java/com/jiaohuan/jiaohuan/MainFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.jiaohuan.jiaohuan.jsonData.UserAPI;
@@ -19,24 +20,21 @@ public class MainFragment extends android.support.v4.app.Fragment {
     private TextView mTextView;
 
     private Button mFriend;
-    private Button mPost;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.main_fragment, container, false);
 
         mFriend = (Button) view.findViewById(R.id.friend);
-
         mTextView = (TextView) view.findViewById(R.id.welcome);
 
         mTextView.setText(CurrentUserObject.getCurrent().getUsername());
 
-        final int account_id = CurrentUserObject.getCurrent().getId();
-
         mFriend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                UserAPI.Factory.getInstance().addFriend(4, 5).enqueue(new Callback<Void>() {
+                UserAPI.Factory.getInstance().addFriend(7, 11).
+                        enqueue(new Callback<Void>() {
                     @Override
                     public void onResponse(Call<Void> call, Response<Void> response) {
                         Log.wtf("SUCCESSFUL", "Request worked");
